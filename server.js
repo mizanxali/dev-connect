3f10065c14b8f61f7848bcd0eb3a83351b8697c4
Mizanali Panjwani <mizanalip786@gmail.com>
2021-02-09 01:28:22 +0530

    Added route files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const connectDB = require('./config/db')
 
 const app = express()
+
 //connect database
 connectDB()
 
@@ -10,6 +11,12 @@ const PORT = process.env.PORT || 5000
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
+
+//define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 app.listen(PORT, () => {
     console.log(`Serving on port ${PORT}`)
