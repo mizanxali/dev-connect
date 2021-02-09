6c0354ce8419d8ab78c5dac790fdc5754c2517d8
Mizanali Panjwani <mizanalip786@gmail.com>
2021-02-09 13:19:25 +0530

    Added github API call to fetch public user repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
+const config = require('config')
 const auth = require('../../middleware/auth')
 const { check, validationResult } = require('express-validator')
 const Profile = require('../../models/Profile')
@@ -234,6 +236,27 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     } catch(err) {
         console.error(err.message)
         res.status(500).send("Server error!")
+    }
+})
+
+// @route   GET api/profile/github/:username
+// @desc    Get user's repositories from GitHub
+// @access  Public
+router.get('/github/:username', async (req, res) => {
+    try {
+        const uri = encodeURI(
+            `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+          );
+          const headers = {
+            'user-agent': 'node.js',
+            Authorization: `token ${config.get('githubToken')}`
+          }
+          
+          const gitHubResponse = await axios.get(uri, { headers })
+          return res.json(gitHubResponse.data)
+    } catch(err) {
+        console.error(err.message)
+        return res.status(404).json({errors: [{ msg: 'No Github profile found' }]})
     }
 })
 
